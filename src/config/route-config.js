bc5ddff87d085225714f9ed9ea0b8ad45949adc8
munjieun <mun.mimi925@gmail.com>
2024-09-16 14:05:50 +0900

    :white_check_mark: deploy add

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,27 +1,28 @@
 import Calendar from "../pages/calendar/Calendar";
 import Layout from "../pages/layout/Layout";
 import Main from "../pages/main/Main";
-
 const { createBrowserRouter } = require("react-router-dom");
 
-
-const router = createBrowserRouter([
-  {
-    path:'/',
-    element:<Layout/>,
-    children: [
-      {
-        path:'/',
-        element:<Main/>
-      },
-      {
-        path:'/calendar',
-        element:<Calendar/>
-      },
-    ]
-  },
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Main />
+        },
+        {
+          path: '/calendar',
+          element: <Calendar />
+        }
+      ]
+    }
+  ],
   {
     basename: "/planner-app"  // GitHub Pages에서 사용하는 repository name
   }
-])
+);
+
 export default router;
