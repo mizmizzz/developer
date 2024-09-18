e2107dfb219bcd603ac5fd48310f78aabe529b97
munjieun <mun.mimi925@gmail.com>
2024-09-18 15:44:44 +0900

    :white_check_mark: router fix

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,8 +1,7 @@
+import { createBrowserRouter } from "react-router-dom";
 import Calendar from "../pages/calendar/Calendar";
 import Layout from "../pages/layout/Layout";
 import Main from "../pages/main/Main";
-const { createBrowserRouter } = require("react-router-dom");
-
 const router = createBrowserRouter(
   [
     {
